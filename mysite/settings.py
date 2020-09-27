3b4d4c5acd5e91fba3a3142789dd5b673afc4838
madussault <admin@madussault.dev>
2020-09-26 21:15:12 -0400

    final version

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,3 +119,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
